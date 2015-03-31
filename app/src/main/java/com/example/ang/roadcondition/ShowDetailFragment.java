93bd10de9bda0696733ac46393152c33075a0493
12301059-ChenJunjun <12301059@bjtu.edu.cn>
2015-03-31 10:22:23 +0800

    12301064-HuTeng
    add three activity about the add collect use case

--- app/src/main/java/com/example/ang/roadcondition/ShowDetailFragment.java
+++ app/src/main/java/com/example/ang/roadcondition/ShowDetailFragment.java
@@ -1,4 +1,4 @@
-package com.example.roadcondition;
+package com.example.ang.roadcondition;
 
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
--- app/src/main/java/com/example/ang/roadcondition/UploadCondition.java
+++ app/src/main/java/com/example/ang/roadcondition/UploadCondition.java
@@ -1,4 +1,4 @@
-package com.example.roadcondition;
+package com.example.ang.roadcondition;
 
 import android.app.Activity;
 import android.os.Bundle;
